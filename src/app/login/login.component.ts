ca3cd265a2e1639abb37129c9564eb99b8751c55
vwez <desirv63@gmail.com>
2022-05-11 09:46:34 +0200

    refactoring login and register

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,6 +4,7 @@ import { Meta, Title } from '@angular/platform-browser';
 import { AuthService } from '../shared/services/auth.service';
 // import { Router } from 'express';
 import { Router } from '@angular/router';
+import  { UserService } from '../shared/services/user.service';
 
 @Component({
   selector: 'app-login',
@@ -11,7 +12,8 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  public loginform: FormGroup;
+
+  // public loginForm: FormGroup;
 
   public descriptionContent = "Trouvez dans votre atypik compte les elements de gestion pour assurer le suivis de vos reservations"
   public pageTitle = "AtypikHouse : accedez à votre atypik compte"
@@ -33,12 +35,13 @@ export class LoginComponent implements OnInit {
     private router:Router,
     private metaService: Meta, 
     private titleService:Title,
+    private userService: UserService
     ) {
 
     this.metaService.addTag({name: 'description', content: this.descriptionContent});
     this.titleService.setTitle(this.pageTitle);
-    this.loginform = this.fb.group({
-      'username': ['', Validators.required],
+    this.loginForm = this.fb.group({
+      'email': ['', Validators.required],
       'password': ['', Validators.required],
       'remember': ['', Validators.required],
 
@@ -49,18 +52,31 @@ export class LoginComponent implements OnInit {
 
   public submit(): void { 
 
-    if (this.loginForm.valid){
-      this.authService.login(this.loginForm.getRawValue()).subscribe(
-        () => {
-        this.router.navigateByUrl('/profil')
-      },
-      (err)=> {
-        this.error = err?.error || 'une erreur est survenue'
-      }
-    )
+    // console.log(this.loginForm.getRawValue())
+    this.userService.login(this.loginForm.getRawValue())
+    // if (this.loginForm.valid){
+    //   this.authService.login(this.loginForm.getRawValue()).subscribe(
+    //     () => {
+    //     this.router.navigateByUrl('/profil')
+    //   },
+    //   (err)=> {
+    //     this.error = err?.error || 'une erreur est survenue'
+    //   }
+    // )
+    
+    // }
+
+    // if (this.loginForm.valid){
+    //   this.userService.login(this.loginForm.value)
+    //     this.router.navigateByUrl('/profil')
+    //   }
+    //   else {
+    //     this.error = 'Login ou mot de passe incorrect'
+    //   }
+    
     }
     
     // console.log(this.loginForm.getRawValue())
-  }
+  // }
 
 }
